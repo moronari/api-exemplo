5d272dada3267878cc013a154524a51a93cc58b6
moronari <moronari1@gmail.com>
2021-10-14 00:46:58 -0300

    feat: adicionando estrutura inicial do projeto kotlin

---
+++ src/main/kotlin/com/functions/apivalidator/ApiValidatorApplication.kt
@@ -0,0 +1,11 @@
+package com.functions.apivalidator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiValidatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiValidatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/functions/apivalidator/ApiValidatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.functions.apivalidator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiValidatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
